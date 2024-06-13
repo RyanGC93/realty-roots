9f0b549cd434c17e50945d913712677b226f7f58
Ryan Conk <ryangconk@gmail.com>
2024-06-13 15:44:22 -0600

    add the properties controller

--- realty-roots-backend/src/properties/properties.controller.ts
+++ realty-roots-backend/src/properties/properties.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get,Param, Post,Body,Put,Delete } from '@nestjs/common';
+import { PropertiesService } from './properties.service';
 
 
 interface CreatePropertyDto {
@@ -14,5 +15,28 @@ interface UpdatePropertyDto {
 
 @Controller('properties')
 export class PropertiesController {
-    
+    constructor(private readonly propertiesService: PropertiesService){}
+
+    @Get()
+    getAllProperties(){    this.propertiesService.getAllProperties()
+    }
+    @Get(':id')
+    getPropertyById(@Param('id') id:number){
+        return this.propertiesService.getPropertyById(Number(id))
+    }
+
+    @Post()
+    createProperty(@Body() createPropertyDto: CreatePropertyDto){
+        return this.propertiesService.createProperty(createPropertyDto)
+    }
+    @Put(':id')
+    updateProperty(@Param('id') id:number, @Body() updatePropertyDro: UpdatePropertyDto ){
+        return this.propertiesService.updateProperty(Number(id),updatePropertyDro)
+    }
+    @Delete(":id")
+    deleteProperty(@Param() id:number){
+        this.propertiesService.deleteProperty(Number(id))
+        return {message: 'Property deleted Succesfully'}
+    }
+
 }
